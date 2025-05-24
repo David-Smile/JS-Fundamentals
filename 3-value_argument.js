55da44b910749e820f6e1e0d2dbfc2f5f9531425
David-Smile <orjidavid18@gmail.com>
2025-05-24 11:35:37 +0100

    added 3-value_argument.js

---
+++ 3-value_argument.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log("No argument");
+}
+else if (args.length === 1) {   
+  console.log("Argument found");
+  console.log(args[0]);
+}
+else {
+  console.log("Arguments found");
+    console.log(args.join(", "));
+}
+// It uses the process.argv array to access the command-line arguments, and the slice method to ignore the first two elements (the node executable and script name).
+// Depending on the number of arguments, it prints a corresponding message to the console.
